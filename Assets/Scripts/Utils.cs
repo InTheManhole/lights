d5fa79edf2de9f02ae31af9f3d0b2972dde2d2ab
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2020-02-29 11:41:57 +0100

    Add utils methods

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,12 @@
+﻿public class Utils {
+
+    public static float LinearCoefficient(int index, int max) {
+        if (index <= (max / 2)) return ((-2 / max) * index) + 1;
+        return ((2 / max) * index) - 1;
+    }
+
+    public static float CurveCoefficient(int index, int max) {
+        float halfMax = max / 2;
+        return ((index / halfMax) - 1) * ((index / halfMax) - 1);
+    }
+}
