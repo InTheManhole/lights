14c0932aeb0322e27efb8c41d82c0194de6c8bad
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2020-02-29 13:55:29 +0100

    Add level manager script

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+    public GameObject groundPrefab;
+
+    public GameObject enemyPrefab;
+
+    public GameObject playerPrefab;
+
+    public GameObject[] obstaclesPrefabs;
+
+    public int size = 50;
+
+
+    private GroundBuilder ground;
+
+    private GameObject player;
+
+
+    void Start() {
+        // Init ground
+        gameObject.AddComponent<GroundBuilder>();
+        ground = gameObject.GetComponent<GroundBuilder>();
+
+        ground.BuildGround(size);
+
+        // Add player
+        player = Instantiate(playerPrefab, new Vector3(0f, 1f, 0f), Quaternion.identity);
+        player.AddComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
