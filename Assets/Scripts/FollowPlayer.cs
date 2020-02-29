2d1395cf331b22fe413b20ea30b05164738737b0
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2020-02-29 12:10:30 +0100

    Add Follow player script

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject player;
+
+    public float offsetX = 0;
+    public float offsetY = 0;
+    public float offsetZ = 0;
+
+    public bool allowRotation = false;
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag(Tags.PLAYER);
+    }
+
+    void LateUpdate() {
+        transform.position = player.transform.position + new Vector3(offsetX, offsetY, offsetZ);
+        if (allowRotation) transform.rotation = player.transform.rotation;
+    }
+}
