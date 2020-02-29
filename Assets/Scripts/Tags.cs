d116a8126007e52a158f7f08c39c938d0fd63a67
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2020-02-29 12:06:40 +0100

    Add tags enum

---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class Tags {
+    static public readonly string PLAYER = "Player";
+    static public readonly string ENEMY = "Enemy";
+    static public readonly string OBSTACLE = "Obstacle";
+    static public readonly string LIGHT = "Light";
+}
